e02c79216bdc02774441e1b5bf7c20c83f8b45e8
daniel5312 <dvh062487@gmail.com>
2026-06-29 00:33:47 -0500

    fix(scripts): agregar argumento de engagement rewards al router inteligente V4

--- contracts/script/AdminConfig.s.sol
+++ contracts/script/AdminConfig.s.sol
@@ -26,7 +26,8 @@ contract AdminConfig is Script {
             0xDd1c12f197E6D1E2FBA15487AaAE500eF6e07BCA, // Mujeres Quenia (G$ - Nueva Tesorería)
             0x0d43131f1577310D6349bAF9D6Da4fC1Cd39764C, // Mujeres Carmen (G$)
             0x9158C35f1a054F25f9D45EA47107D54a2ea25945, // Pool Login Wallet (5% CELO)
-            0x9bc43f955ce11948e4fD6EAC28d46875Fba9f5F9  // Biota Productores (95% CELO)
+            0x9bc43f955ce11948e4fD6EAC28d46875Fba9f5F9, // Biota Productores (95% CELO)
+            0x25db74CF4E7BA120526fd87e159CF656d94bAE43  // Engagement Rewards (GoodDollar Claim)
         );
 
         vm.stopBroadcast();
